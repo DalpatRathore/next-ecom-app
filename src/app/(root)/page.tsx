027b5922d2c4a950c68d05cec810ada986e836d7
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-24 19:43:18 +0530

    feat: billboard hero section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
-
-export default function Home() {
+import { getBillboard } from "@/actions/getBillboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/Container";
+export const revalidate = 0;
+export default async function Home() {
+  const billboard = await getBillboard("929f7749-0e8a-431c-81e0-11eab26b6e27");
   return (
-    <main>
-      <h1>Dalpat Rathore</h1>
-    </main>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}></Billboard>
+      </div>
+    </Container>
   );
 }
