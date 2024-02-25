0aca3889756682944c736c7c7274b30674af469a
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-25 12:32:19 +0530

    feat: product info section

--- src/app/(routes)/product/[productId]/page.tsx
+++ src/app/(routes)/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import { getProduct } from "@/actions/getProduct";
 import { getProducts } from "@/actions/getProducts";
 import Container from "@/components/Container";
+import Info from "@/components/Info";
 import ProductList from "@/components/ProductList";
 import Gallery from "@/components/gallery";
 
@@ -20,7 +21,9 @@ const ProductIdPage = async ({ params }: ProductIdPageProps) => {
         <div className="px-4 py-10 smpx-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <Gallery images={product.images}></Gallery>
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info product={product} />
+            </div>
           </div>
         </div>
         <hr className="my-10" />
